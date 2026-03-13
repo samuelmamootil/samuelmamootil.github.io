88bb2d27b3c9cb30920e4a0c35cb6e6bba3cfd79
samuelmamootil <mamootilsamuel1@gmail.com>
2026-03-12 22:51:19 -0500

    fix: remove toggle, force light theme

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -4,9 +4,6 @@ const drawer = document.getElementById("navDrawer");
 if (toggle && drawer) {
   toggle.addEventListener("click", () => drawer.classList.toggle("open"));
 }
-
-// Close drawer on link click
 document.querySelectorAll(".nav__drawer a").forEach((a) => {
   a.addEventListener("click", () => drawer.classList.remove("open"));
 });
-
